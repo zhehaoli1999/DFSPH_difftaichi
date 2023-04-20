c91866489742087dc261784f1f1f04f4d2556d39
xqy <liamxqy@163.com>
2023-04-20 10:54:52 +0800

    update trial

--- trial/diff-taichi-trial19.py
+++ trial/diff-taichi-trial19.py
@@ -4,7 +4,8 @@ ti.init(arch=ti.cpu)
 
 x = ti.field(dtype=float, needs_dual=True)
 y = ti.field(dtype=float, needs_dual=True)
-ti.root.place(x, x.dual, y, y.dual)
+z = ti.field(dtype=float, needs_dual=True)
+ti.root.place(x, x.dual, y, y.dual, z, z.dual)
 
 loss = ti.field(dtype=float, shape=(), needs_dual=True)
 
@@ -12,18 +13,20 @@ loss = ti.field(dtype=float, shape=(), needs_dual=True)
 def init():
     x[None] = 1.0
     y[None] = 0.0
+    z[None] = 0.0
     loss[None] = 0.0
 
 @ti.kernel
 def func():
     print_result()
-    x[None] = x[None] + x[None] * y[None]
+    z[None] = x[None]
     print_result()
-    y[None] = y[None] + x[None]
+    x[None] = z[None] + z[None] * y[None]
     print_result()
 
 @ti.kernel
 def get_loss():
+    print_result()
     loss[None] = x[None] + y[None]
     print_result()
 
@@ -32,6 +35,7 @@ def print_result():
     print("------------------------------------------------")
     print(f"x: {x[None]}, {x.dual[None]}")
     print(f"y: {y[None]}, {y.dual[None]}")
+    print(f"z: {z[None]}, {z.dual[None]}")
     print(f"loss: {loss[None]}, {loss.dual[None]}")
     print("------------------------------------------------")
 
